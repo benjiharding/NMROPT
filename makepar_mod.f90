50a2fc3cdd57906f11d72473839f08621f1398d1
Ben Harding <ben.harding@resmodsol.com>
2023-03-10 11:44:36 -0800

    remove nst

--- makepar_mod.f90
+++ makepar_mod.f90
@@ -14,7 +14,6 @@ contains
       write (99, "(A)") 'START OF PARAMETERS:'
       write (99, "(A)") 'data.dat                          - file with data'
       write (99, "(A)") '1 4 5 6 7 11                      - columns for dh, x, y, z, var and wt'
-      write (99, "(A)") '1                                 - normal score transform var? (0=no, 1=yes)'
       write (99, "(A)") '-1.0e21    1.0e21                 - trimming limits'
       write (99, "(A)") '100                               - number of unconditional realizations'
       write (99, "(A)") '50      0.5    1.0                - grid definition: nx,xmn,xsiz'
